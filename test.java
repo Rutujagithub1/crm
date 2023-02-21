2969d240fa10134c0ae42ce6ef53dda0876f54a4
Rutujagithub1 <125717168+Rutujagithub1@users.noreply.github.com>
2023-02-21 13:11:16 +0530

    Add files via upload

---
+++ test.java
@@ -0,0 +1,15 @@
+package Basic;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class test 
+{
+	public static void main(String[] args)
+	{
+    ChromeDriver driver=new ChromeDriver();
+    driver.get("https://www.flipkart.com/");
+    driver.get("https://www.selenium.dev/downloads/");
+    driver.get("https://www.instagram.com/");
+    
+} 
+}
